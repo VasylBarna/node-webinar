94181ed24b6c3c5f13ed01372d6f33017862bc43
VasylBarna <vasylbarna@gmail.com>
2021-11-23 17:47:19 +0200

    weather

--- index.js
+++ index.js
@@ -1,76 +1,54 @@
-// const http = require("http");
-// const fs = require("fs").promises;
+const express = require('express')
+const morgan = require('morgan')
+const got = require('got')
+const cors = require('cors')
 
-// const PORT = 8081;
-// const requestHandler = async (request, response) => {
-//   const manifest = await fs.readFile("./package.json", "utf8");
-//   //   response.writeHead(200, { "Content-type": "text/html" });
-//   //   response.end("<h1>GOIT</h1>");
-//   //   response.writeHead(200, { "Content-type": "text/json" });
-//   //   response.end(JSON.stringify({ a: 1, b: [] }));
+require('dotenv').config()
+const app = express()
 
-//   //   if (request.url.indexOf("/home") >= 0) {
-//   //     response.writeHead(200, { "Content-type": "text/json" });
-//   //     return response.end('{"url": "homepage"}');
-//   //   }
-//   response.writeHead(200, { "Content-type": "text/json" });
-//   return response.end(manifest);
-// };
+const PORT = process.env.PORT || 8081
+const baseUrl = 'http://api.weatherbit.io/v2.0/current'
+const apiKey = process.env.WEATHER_API_KEY
 
-// const server = http.createServer(requestHandler);
+app.use(morgan('tiny'))
+app.use(cors())
 
-// server.listen(PORT, (err) => {
-//   if (err) {
-//     console.error(" Error at aserver launch: ", err);
-//   }
-//   console.log(`Server works at port ${PORT}!`);
-// });
-
-// то самое но через Express:
-
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const PORT = 8081;
-app.use(express.json());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true })); //парсить текст з html
-app.use(morgan("tiny"));
-
-// app.use((req, res, next) => {
-//   console.log(`${req.method}, ${req.originalUrl}, ${new Date().toISOString()}`);
-//   next();
-// });
-app.post("/home", (req, res) => {
-  if (!req.body.GOIT) {
-    return res.status(400).json({ status: "goit parameter" });
+app.get('/api/weather', async (req, res) => {
+  try {
+    const { latitude, longitude } = req.query
+    if (!latitude) {
+      return res
+        .status(400)
+        .json({ message: 'latitude parameter is mandatory' })
+    }
+    if (!longitude) {
+      return res
+        .status(400)
+        .json({ message: 'longitude parameter is mandatory' })
+    }
+    const response = await got(baseUrl, {
+      searchParams: {
+        key: apiKey,
+        lat: latitude,
+        lon: longitude,
+      },
+      responseType: 'json',
+    })
+    const [weatherData] = response.body.data
+    const {
+      city_name,
+      weather: { description },
+      temp,
+    } = weatherData
+    res.json({ city_name, description, temp })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
   }
-  console.log(req.body);
-  res.json({ javascript: "object", body: req.body });
-});
+})
 
-// app.get("/home", (req, res) => {
-//   res.status(500).json({ javascript: "object" });
-// });
-
-// app.get("/home", (req, res) => {
-//   res.send("get request");
-// });
-
-// app.post("/home", (req, res) => {
-//   res.send("post request");
-// });
-
-// app.delete("/home", (req, res) => {
-//   res.send("delete request");
-// });
-
-// app.use((req, res) => {
-//   res.send("middleware request");
-// });
 app.listen(PORT, (err) => {
   if (err) {
-    console.error(" Error at aserver launch: ", err);
+    console.error(' Error at aserver launch: ', err)
   }
-  console.log(`Server works at port ${PORT}!`);
-});
+  console.log(`Server works at port ${PORT}!`)
+})
